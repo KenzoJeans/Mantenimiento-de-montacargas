0b455d36aefb73a5ad1915d2c5c914d2a609cdbe
Ambiental Kenzo Jeans <ambientalkenzo@gmail.com>
2026-06-23 08:13:04 -0500

    app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ json_data = json.dumps(historial_mantenimiento)
 
 # 2. CONFIGURA AQUÍ TU URL DE GITHUB
 # Mientras lo subes, el código usará un modelo alternativo de pruebas para no fallar
-URL_TU_MODELO_GLB = "https://github.com/KenzoJeans/Mantenimiento-de-montacargas/raw/refs/heads/main/forklift_low_poly.glb" 
+URL_TU_MODELO_GLB = "https://raw.githubusercontent.com/KenzoJeans/Mantenimiento-de-montacargas/refs/heads/main/forklift_low_poly.glb" 
 # REEMPLAZA por tu link de GitHub cuando esté listo:
 # URL_TU_MODELO_GLB = "https://raw.githubusercontent.com/tu_usuario/tu_repo/main/assets/forklift_low_poly.glb"
 
